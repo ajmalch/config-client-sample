282a21a928516f7bb4acb20dc50460665c171154
ajmalch <ajmalch@gmail.com>
2018-03-30 21:13:26 -0400

    Updated to heroku and added logging

--- src/main/java/com/example/configclientsample/ConfigClientSampleApplication.java
+++ src/main/java/com/example/configclientsample/ConfigClientSampleApplication.java
@@ -1,5 +1,6 @@
 package com.example.configclientsample;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController("/")
+@Slf4j
 public class ConfigClientSampleApplication {
 
 	@Value("${message}")
@@ -21,6 +23,7 @@ public class ConfigClientSampleApplication {
 
 	@GetMapping("sayHi")
 	public  String sayHi(){
+		log.info("Hi Called");
 		return message;
 	}
 
